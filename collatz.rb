6bf86b122db3b722325da8f64ec2873808a58b3c
Drew Hartberg <drewhartberg@gmail.com>
2019-12-27 18:45:29 +0000

    result

--- collatz.rb
+++ collatz.rb
@@ -1,17 +1,34 @@
+require "byebug" 
+
+# def collatz(n, chain = [n])
+#  return 1 if n == 1
+#   if n % 2 == 0
+#     chain << n/2
+#     num = collatz(n/2, chain)
+#   else 
+#     chain << n * 3 + 1
+#     num = collatz(n * 3 + 1, chain)
+#   end
+#   debugger
+#   chain.each do |x|
+#     print x.to_s + " -> "
+#   end
+# end
+
 def collatz(n)
-  return n if n == 1
-  chain = []
+  print n.to_s + " -> "
+ return 1 if n == 1
   if n % 2 == 0
-    chain << n/2
+    num = collatz(n/2)
+    return
   else 
-    chain << n * 3 + 1 
-  collatz 
-  end 
-  puts chain.to_s.split("->")
+    num = collatz(n * 3 + 1)
+    return
+  end
+  # debugger
 end
 
-
-collatz(20)
+collatz(5)
 
 
 
